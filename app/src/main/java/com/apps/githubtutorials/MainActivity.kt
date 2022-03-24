5c4c2d021cb48d7ba7e629e023dfa71a8c88a8a2
madahetooo <ieslammedhat@gmail.com>
2022-03-24 18:08:02 +0200

    fifth master commit

--- app/src/main/java/com/apps/githubtutorials/MainActivity.kt
+++ app/src/main/java/com/apps/githubtutorials/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("This is another commit")
+        print("This is another commit") // default
+        print("this is latest master commit") // master
     }
 }
